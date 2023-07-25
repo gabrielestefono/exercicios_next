908df87c690db6cd6ba4194107f775da1c9dbe34
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-25 01:06:51 -0300

    Integração com CSS #01

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,10 @@
+// Não posso importar o CSS aqui, tem que ser feito no App.css
+export default function integracao(){
+    return(
+        <div className="integracao1">
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branco">Texto #03</div>
+        </div>
+    )
+}
