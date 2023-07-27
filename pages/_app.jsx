5ffec18885c052b0fa115dbcd93c0ab456355bf5
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-27 19:41:18 -0300

    Estilo condicional

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import "./css/integracao1.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/css/usandoestilo.jsx
@@ -0,0 +1,10 @@
+import Estilo from "@/components/Estilo";
+
+export default function UsandoEstilo(){
+    return(
+        <>
+            <Estilo numero={-3} color="#000" direita={true}/>
+            <Estilo numero={13} color="#fff" direita/>
+        </>
+    )
+}
